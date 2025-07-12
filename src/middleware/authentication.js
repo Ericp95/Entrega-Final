c6c9e7cde5bf61570813aaddb67f42564973222e
Ericp95 <ericp6960@gmail.com>
2025-07-12 01:05:50 -0300

    arreglo errores

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -4,42 +4,20 @@ import 'dotenv/config';
 
   const secret_key = process.env.JWT_SECRET_KEY;
  
-  // Middleware para verificar el token JWT
-/* export const authentication = (req, res, next) => {
-    //const token = req.headers['authorization'].split(" ")[1]; //.split(" ")[1]
 
+export const authentication = (req, res, next) => {
     const authHeader = req.headers.authorization;
-if (!authHeader) {
-  return res.status(401).json({ mensaje: 'No se proporcionó el token' });
-}
 
-const token = authHeader.split(' ')[1];
-  
+    if (!authHeader) {
+        return res.status(401).json({ mensaje: 'No se proporcionó el token' });
+    }
+
+    const token = authHeader.split(' ')[1];
 
     if (!token) return res.sendStatus(401);
 
-
     jwt.verify(token, secret_key, (err) => {
-        if (err) return res.sendStatus(403);
+        if (err) return res.status(403).json({ mensaje: err.message });
         next();
     });
-}*/
-
-/*export const authentication = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-
-    if (!authHeader) {
-        return res.status(401).json({ message: 'Authorization header missing' });
-    }
-
-    const token = authHeader.split("-")[1];
-
-    if (!token) {
-        return res.status(401).json({ message: 'Token missing' });
-    }
-
-    jwt.verify(token, secret_key, (err) => {
-        if (err) return res.sendStatus(403);
-        next();
-    });
-}*/
+};
