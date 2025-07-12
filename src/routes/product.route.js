aee6dbce5472f7ffd55de01feeba1a0e9d27ae84
Ericp95 <ericp6960@gmail.com>
2025-07-12 01:06:01 -0300

    arreglo errores

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,16 +1,17 @@
 //router
 import { Router } from "express";
 import productController from "../controllers/product.controller.js";
+import { authentication } from "../middleware/authentication.js";
 //import { authentication } from "../middleware/auth.middleware.js";
 
 
 const router = Router();
 
-router.get("/",productController.getProducts);
-router.get("/:id", productController.getOneProducts);
+router.get("/",authentication,productController.getProducts);
+router.get("/:id", authentication, productController.getOneProducts);
 
-router.post("/create", productController.createProduct);
-router.delete("/:id", productController.deleteProducts);
+router.post("/create", authentication, productController.createProduct);
+router.delete("/:id",authentication, productController.deleteProducts);
 
 
 
